81e4c2fe81129e5f7a8374a2435e69191076ed23
Marcus Kullman <marcus@marcuskullman.com>
2024-03-06 16:22:02 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import styles from "./app.module.scss"
+
+export default function App() {
+  return (
+    <div className={styles.app}>
+      <h1>Penis</h1>
+    </div>
+  )
+}
