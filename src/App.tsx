5a3e6d7307f3a8423a526197d174b85c57c7a0b9
Marcus Kullman <marcus@marcuskullman.com>
2024-03-12 13:08:38 +0100

    fixar

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ export default function App() {
   return (
     <div className={styles.app}>
       <p>
-        Centered on new ideas and bespoke solutions for websites, identities,
-        books, packaging, etc.
+        Design and development of websites, identities, books, packaging, etc.
         <br />
         Based in Stockholm, Östermalm.{" "}
         <a href="mailto:nicklas@nicklaskullman.se" className={styles.link}>
